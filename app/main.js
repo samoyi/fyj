65b1a01dc2be6f3bf131f6bb2545608930264b63
samoyi <greatlining@163.com>
2017-06-27 20:26:34 +0800

    init

---
+++ app/main.js
@@ -0,0 +1,7 @@
+
+import Vue from "Vue";
+import "./common.scss";
+
+let a = 3344534343;
+
+console.log(a);
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+let webpack = require('webpack');
+let HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+    devtool: "source-map",
+
+    entry: __dirname + "/app/main.js",
+    output: {
+        path: __dirname + "/build",
+        filename: "bundle.js",
+        // filename: "bundle-[hash].js",
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+                query: {
+                    presets: ["es2015"],
+                },
+            },
+            {
+    			test: /\.scss$/,
+    			loader: "style-loader!css-loader!sass-loader",
+    		},
+        ],
+    },
+
+    devServer: {
+        contentBase: "./build", // 本地服务器所加载的页面所在的目录
+        color: true, // 终端中输出结果为彩色
+        historyApiFallback: true, // 不跳转
+        inline: true, // 实时刷新
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: __dirname + "/app/index.tmpl.html"
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+             sourceMap: true, // 压缩后仍然显示source map
+        }),
+    ]
+};
